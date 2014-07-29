5639fce695657a29787b38083b4271d28cce012f
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2014-07-29 10:41:33 -0700

    add new stress test

--- stress_test.go
+++ stress_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/cloudfoundry-incubator/executor/client"
 	"github.com/cloudfoundry-incubator/inigo/inigo_server"
 	Bbs "github.com/cloudfoundry-incubator/runtime-schema/bbs"
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
 	"github.com/cloudfoundry-incubator/runtime-schema/models/factories"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -39,7 +40,41 @@ var _ = Describe("Stress tests", func() {
 			suiteContext.RepRunner.Start()
 		})
 
-		It("should be able to run many tasks without issue", func() {
+		It("should be able to gently ramp up to handle many tasks without issue", func() {
+			c := client.New(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", suiteContext.ExecutorPort))
+			nTasks := 100
+			totalResources, _ := c.TotalResources()
+			vizzini.Printf("\nTotal resources: %s\n", format.Object(totalResources, 0))
+			remainingResources, _ := c.RemainingResources()
+			vizzini.Printf("\nRemaining resources: %s\n", format.Object(remainingResources, 0))
+
+			for j := 0; j < nTasks; j++ {
+				guid := factories.GenerateGuid()
+				task := models.Task{
+					Guid:     guid,
+					MemoryMB: 10,
+					DiskMB:   10,
+					Actions: []models.ExecutorAction{
+						{Action: models.RunAction{
+							Path: "bash",
+							Args: []string{"-c", fmt.Sprintf("curl %s; sleep 1000", strings.Join(inigo_server.CurlArgs(guid), " "))},
+						}},
+					},
+					Stack: suiteContext.RepStack,
+				}
+
+				vizzini.Printf("Scheduling %d [%s]...\n", j+1, task.Guid)
+				err := bbs.DesireTask(task)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				Eventually(inigo_server.ReportingGuids).Should(ContainElement(guid))
+
+				remainingResources, _ := c.RemainingResources()
+				vizzini.Printf("\n...#%d is up.  Remaining resources: %s\n", j+1, format.Object(remainingResources, 0))
+			}
+		})
+
+		It("should be able to run many tasks simultaneously without issue", func() {
 			c := client.New(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", suiteContext.ExecutorPort))
 			nRounds := 10
 			nTasks := 100
@@ -50,13 +85,20 @@ var _ = Describe("Stress tests", func() {
 				vizzini.Printf("\nRemaining resources: %s\n", format.Object(remainingResources, 0))
 
 				for j := 0; j < nTasks; j++ {
-					task := factories.BuildTaskWithRunAction(
-						suiteContext.RepStack,
-						10,
-						10,
-						"bash",
-						[]string{"-c", fmt.Sprintf("curl %s; sleep 2", strings.Join(inigo_server.CurlArgs(factories.GenerateGuid()), " "))},
-					)
+					guid := factories.GenerateGuid()
+					task := models.Task{
+						Guid:     guid,
+						MemoryMB: 10,
+						DiskMB:   10,
+						Actions: []models.ExecutorAction{
+							{Action: models.RunAction{
+								Path: "bash",
+								Args: []string{"-c", fmt.Sprintf("curl %s; sleep 2", strings.Join(inigo_server.CurlArgs(guid), " "))},
+							}},
+						},
+						Stack: suiteContext.RepStack,
+					}
+
 					vizzini.Printf("Scheduling %d [%s]...\n", i*nTasks+j+1, task.Guid)
 					err := bbs.DesireTask(task)
 					Ω(err).ShouldNot(HaveOccurred())
