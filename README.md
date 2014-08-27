19387b6ab0633963bee3509d9cb85b2431dd22a6
pivotal <pivotal@metson.sf.pivotallabs.com>
2014-08-27 15:21:20 -0700

    add test to accept environment variables at the container level in garden

--- README.md
+++ README.md
@@ -6,5 +6,13 @@ Inconceivable tests!
 
 These are "beta" versions of some interesting Inigo-style Diego tests.
 
+## What's In Here
+
+- Under `/acceptance` are some tests Onsi's written to help accept stories.  They tend to only work with bosh-lite and tend to be very low-level, side-stepping things like CC and etcd and the executor and talking directly to components via NATS or http.
+
+- Under `/blackbox` is a stress test that runs against bosh-lite.  It uses [veritas](https://github.com/cloudfoundry-incubator/veritas) and [cf](https://github.com/cloudfoundry/cli) to push apps to a local bosh-lite, scale them up, then scale them down.  It emits goroutine counts, etc. of all the components using `veritas vitals`.
+
+- Under the root directory are some inigo-style tests.  These aren't particularly well-maintained and are mostly proving grounds for ideas.
+
 ####Learn more about Diego and its components at [diego-design-notes](https://github.com/cloudfoundry-incubator/diego-design-notes)
 
--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -1,11 +1,13 @@
 package acceptance_test
 
 import (
+	"io"
+
 	"github.com/cloudfoundry-incubator/garden/client"
 	"github.com/cloudfoundry-incubator/garden/client/connection"
 	"github.com/cloudfoundry-incubator/garden/warden"
-	"github.com/cloudfoundry-incubator/runtime-schema/cc_messages"
-	"github.com/cloudfoundry/yagnats"
+	"github.com/onsi/gomega/gbytes"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -39,6 +41,42 @@ var _ = Describe("Acceptance", func() {
 			}
 
 			Ω(results).Should(ConsistOf(BeNil(), HaveOccurred()))
+		})
+
+		It("should support setting environment variables on the container (#77303456)", func() {
+			container, err := gardenClient.Create(warden.ContainerSpec{
+				Handle: "cap'n-planet",
+				Env: []string{
+					"ROOT_ENV=A",
+					"OVERWRITTEN_ENV=B",
+					"HOME=/nowhere",
+				},
+			})
+			Ω(err).ShouldNot(HaveOccurred())
+
+			buffer := gbytes.NewBuffer()
+			process, err := container.Run(warden.ProcessSpec{
+				Path: "bash",
+				Args: []string{"-c", "printenv"},
+				Env: []string{
+					"OVERWRITTEN_ENV=C",
+				},
+			}, warden.ProcessIO{
+				Stdout: io.MultiWriter(buffer, GinkgoWriter),
+				Stderr: io.MultiWriter(buffer, GinkgoWriter),
+			})
+
+			Ω(err).ShouldNot(HaveOccurred())
+
+			process.Wait()
+
+			gardenClient.Destroy("cap'n-planet")
+
+			Ω(buffer.Contents()).Should(ContainSubstring("OVERWRITTEN_ENV=C"))
+			Ω(buffer.Contents()).ShouldNot(ContainSubstring("OVERWRITTEN_ENV=B"))
+			Ω(buffer.Contents()).Should(ContainSubstring("HOME=/home/vcap"))
+			Ω(buffer.Contents()).ShouldNot(ContainSubstring("HOME=/nowhere"))
+			Ω(buffer.Contents()).Should(ContainSubstring("ROOT_ENV=A"))
 		})
 
 		It("should mount an ubuntu docker image, just fine", func() {
@@ -79,78 +117,6 @@ var _ = Describe("Acceptance", func() {
 			process.Wait()
 
 			gardenClient.Destroy("my-none-ubuntu-based-docker-image")
-		})
-	})
-
-	Describe("Docker", func() {
-		var natsClient *yagnats.Client
-		BeforeEach(func() {
-			natsClient = yagnats.NewClient()
-			err := natsClient.Connect(&yagnats.ConnectionInfo{
-				Addr:     "10.244.0.6.xip.io:4222",
-				Username: "nats",
-				Password: "nats",
-			})
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-
-		It("should immediately respond to diego.docker.staging.start", func() {
-			payload := make(chan []byte)
-			_, err := natsClient.Subscribe("diego.docker.staging.finished", func(message *yagnats.Message) {
-				payload <- message.Payload
-			})
-			Ω(err).ShouldNot(HaveOccurred())
-
-			err = natsClient.Publish("diego.docker.staging.start", []byte(`{
-      "app_id": "some-app-guid",
-      "task_id": "some-task-guid"
-    }`))
-			Ω(err).ShouldNot(HaveOccurred())
-
-			Eventually(payload).Should(Receive(MatchJSON(`{
-      "app_id": "some-app-guid",
-      "task_id": "some-task-guid"
-    }`)))
-		})
-
-		It("should run a (heavy) docker-based app", func() {
-			desireMessage := cc_messages.DesireAppRequestFromCC{
-				ProcessGuid:    "my-grace-docker-app",
-				DockerImageUrl: "docker:///onsi/grace",
-				Stack:          "lucid64",
-				StartCommand:   "/grace",
-				Environment: cc_messages.Environment{
-					{Name: "custom-env", Value: "grace-upon-grace"},
-				},
-				Routes:       []string{"docker-grace"},
-				MemoryMB:     128,
-				DiskMB:       1024,
-				NumInstances: 1,
-				LogGuid:      "docker-grace",
-			}
-
-			err := natsClient.Publish("diego.docker.desire.app", desireMessage.ToJSON())
-			Ω(err).ShouldNot(HaveOccurred())
-		})
-
-		It("should run a (light-weight) docker-based app", func() {
-			desireMessage := cc_messages.DesireAppRequestFromCC{
-				ProcessGuid:    "my-lite-grace-docker-app",
-				DockerImageUrl: "docker:///onsi/grace-busybox",
-				Stack:          "lucid64",
-				StartCommand:   "/grace",
-				Environment: cc_messages.Environment{
-					{Name: "custom-env", Value: "grace-upon-grace"},
-				},
-				Routes:       []string{"lite-docker-grace"},
-				MemoryMB:     128,
-				DiskMB:       1024,
-				NumInstances: 1,
-				LogGuid:      "lite-docker-grace",
-			}
-
-			err := natsClient.Publish("diego.docker.desire.app", desireMessage.ToJSON())
-			Ω(err).ShouldNot(HaveOccurred())
 		})
 	})
 })
