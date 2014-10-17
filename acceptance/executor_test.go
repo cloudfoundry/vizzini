5393d4b09cd9645b238806a7bf14ac5a5662f3e3
Onsi Fakhouri <onsijoe@gmail.com>
2014-10-17 16:43:14 -0700

    add executor test to acceptance

---
+++ acceptance/executor_test.go
@@ -0,0 +1,81 @@
+package acceptance_test
+
+import (
+	"net/http"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
+
+	"github.com/cloudfoundry-incubator/executor/api"
+	"github.com/cloudfoundry-incubator/executor/client"
+)
+
+var _ = Describe("Executor", func() {
+	var c api.Client
+
+	BeforeEach(func() {
+		c = client.New(http.DefaultClient, "http://10.244.17.2.xip.io:1700")
+		c.DeleteContainer("download-container")
+	})
+
+	It("should handle .tgz", func() {
+		_, err := c.AllocateContainer("download-container", api.ContainerAllocationRequest{
+			MemoryMB: 1024,
+			DiskMB:   10240,
+		})
+		Ω(err).ShouldNot(HaveOccurred())
+
+		_, err = c.InitializeContainer("download-container", api.ContainerInitializationRequest{})
+		Ω(err).ShouldNot(HaveOccurred())
+
+		c.Run("download-container", api.ContainerRunRequest{
+			Actions: []models.ExecutorAction{
+				{models.DownloadAction{
+					From:     "http://onsi-public.s3.amazonaws.com/foo.tar.gz",
+					To:       "/tmp/foo-1",
+					CacheKey: "container-cache-key-tar-gz",
+				}},
+			},
+		})
+	})
+
+	It("should be performant", func() {
+		_, err := c.AllocateContainer("download-container", api.ContainerAllocationRequest{
+			MemoryMB: 1024,
+			DiskMB:   10240,
+		})
+		Ω(err).ShouldNot(HaveOccurred())
+
+		_, err = c.InitializeContainer("download-container", api.ContainerInitializationRequest{})
+		Ω(err).ShouldNot(HaveOccurred())
+
+		c.Run("download-container", api.ContainerRunRequest{
+			Actions: []models.ExecutorAction{
+				models.Parallel([]models.ExecutorAction{
+					{models.DownloadAction{
+						From:     "http://onsi-public.s3.amazonaws.com/foo.zip",
+						To:       "/tmp/foo-1",
+						CacheKey: "container-cache-key-1",
+					}},
+					{models.DownloadAction{
+						From:     "http://onsi-public.s3.amazonaws.com/foo.zip",
+						To:       "/tmp/foo-2",
+						CacheKey: "container-cache-key-2",
+					}},
+					{models.DownloadAction{
+						From:     "http://onsi-public.s3.amazonaws.com/foo.zip",
+						To:       "/tmp/foo-3",
+						CacheKey: "container-cache-key-3",
+					}},
+					{models.DownloadAction{
+						From:     "http://onsi-public.s3.amazonaws.com/foo.zip",
+						To:       "/tmp/foo-4",
+						CacheKey: "container-cache-key-4",
+					}},
+				}...),
+			},
+		})
+	})
+})
