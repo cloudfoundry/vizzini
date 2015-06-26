beae3f3f59713bd3bb9d585cde594c117fc7d02d
Eric Malm <emalm@pivotal.io>
2015-06-26 16:08:19 -0700

    Run action as pre-existing user in rootfs

---
+++ user_test.go
@@ -0,0 +1,47 @@
+package vizzini_test
+
+import (
+	. "github.com/pivotal-cf-experimental/vizzini/matchers"
+
+	"github.com/cloudfoundry-incubator/receptor"
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Users", func() {
+	var task receptor.TaskCreateRequest
+
+	Context("{DOCKER} with an existing 'alice' user in the rootfs", func() {
+		BeforeEach(func() {
+			task = TaskWithGuid(guid)
+
+			// TODO: REMOVE once #95582718 delivered, so that garden-linux in diego-release supports user namespaces correctly
+			task.Privileged = true
+
+			task.RootFS = "docker:///cloudfoundry/busybox-alice"
+			task.Action = &models.RunAction{
+				Path: "sh",
+				Args: []string{"-c", "echo foo; whoami > /tmp/output"},
+				User: "alice",
+			}
+			task.ResultFile = "/tmp/output"
+
+			Ω(client.CreateTask(task)).Should(Succeed())
+		})
+
+		It("runs an action as alice", func() {
+			Eventually(TaskGetter(guid)).Should(HaveTaskState(receptor.TaskStateRunning))
+			Eventually(TaskGetter(guid), 10).Should(HaveTaskState(receptor.TaskStateCompleted))
+
+			task, err := client.GetTask(guid)
+			Ω(err).ShouldNot(HaveOccurred())
+
+			Ω(task.Failed).Should(BeFalse())
+			Ω(task.Result).Should(ContainSubstring("alice"))
+
+			Ω(client.DeleteTask(guid)).Should(Succeed())
+		})
+	})
+})
