b1b1b1b40e4ade606a22f8550ec7c14c2692674d
Brandon Shroyer <bshroyer@pivotal.io>
2017-01-16 11:43:03 -0500

    Remove test for cell name and availability zone.

--- cells_test.go
+++ cells_test.go
@@ -1,9 +1,6 @@
 package vizzini_test
 
 import (
-	"strings"
-
-	"code.cloudfoundry.org/bbs/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -14,20 +11,12 @@ var _ = Describe("Cells", func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(len(cells)).To(BeNumerically(">=", 1))
 
-		var cell_z1_0 *models.CellPresence
-		for _, cell := range cells {
-			if strings.HasPrefix(cell.CellId, "cell_z1-0") {
-				cell_z1_0 = cell
-				break
-			}
-		}
+		cell0 := cells[0]
 
-		Expect(cell_z1_0).NotTo(BeNil())
-		Expect(cell_z1_0.CellId).To(HavePrefix("cell_z1-0"))
-		Expect(cell_z1_0.Zone).To(Equal("z1"))
-		Expect(cell_z1_0.Capacity.MemoryMb).To(BeNumerically(">", 0))
-		Expect(cell_z1_0.Capacity.DiskMb).To(BeNumerically(">", 0))
-		Expect(cell_z1_0.Capacity.Containers).To(BeNumerically(">", 0))
-		Expect(len(cell_z1_0.RootfsProviders)).To(BeNumerically(">", 0))
+		Expect(cell0).NotTo(BeNil())
+		Expect(cell0.Capacity.MemoryMb).To(BeNumerically(">", 0))
+		Expect(cell0.Capacity.DiskMb).To(BeNumerically(">", 0))
+		Expect(cell0.Capacity.Containers).To(BeNumerically(">", 0))
+		Expect(len(cell0.RootfsProviders)).To(BeNumerically(">", 0))
 	})
 })
