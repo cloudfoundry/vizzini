80b33fb95a469d5a4eabd1949542f446821d2b74
Tiago Scolari <tscolari@pivotal.io>
2017-06-08 11:37:03 +0100

    Add support for `placement-tags`
    New optional flag `--rep-placement-tag` to tag LRPs and tasks
    
    [closes #2]
    
    Signed-off-by: Claudia Beresford <cberesford@pivotal.io>

--- vizzini_suite_test.go
+++ vizzini_suite_test.go
@@ -1,6 +1,7 @@
 package vizzini_test
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -34,6 +35,7 @@ var (
 	bbsAddress           string
 	bbsClientCert        string
 	bbsClientKey         string
+	repPlacementTags     multiArgList
 	routableDomainSuffix string
 	sshAddress           string
 	sshHost              string
@@ -57,6 +59,7 @@ func init() {
 	flag.StringVar(&routableDomainSuffix, "routable-domain-suffix", "bosh-lite.com", "suffix to use when constructing FQDN")
 	flag.StringVar(&hostAddress, "host-address", "10.0.2.2", "address that a process running in a container on Diego can use to reach the machine running this test.  Typically the gateway on the vagrant VM.")
 	flag.BoolVar(&enableDeclarativeHealthCheck, "enable-declarative-healthcheck", false, "true if the rep is configured to prefer declarative healthchecks, false otherwise")
+	flag.Var(&repPlacementTags, "rep-placement-tag", "rep placement tag, can be set more than once")
 	flag.Parse()
 
 	if bbsAddress == "" {
@@ -145,4 +148,19 @@ func initializeBBSClient() bbs.InternalClient {
 	bbsClient, err := bbs.NewSecureSkipVerifyClient(bbsAddress, bbsClientCert, bbsClientKey, 0, 0)
 	Expect(err).NotTo(HaveOccurred())
 	return bbsClient
+}
+
+type multiArgList []string
+
+func (p *multiArgList) String() string {
+	return fmt.Sprintf("%v", *p)
+}
+
+func (p *multiArgList) Set(value string) error {
+	if value == "" {
+		return errors.New("Cannot set blank value")
+	}
+
+	*p = append(*p, value)
+	return nil
 }
