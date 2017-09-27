29e20c03f85bedabc0202c6c70a080b43d5e9894
Danny Cao <dcao@us.ibm.com>
2017-09-27 12:11:33 -0400

    Add vizzini test to ensure TLS termination is done properly in envoy
    [#149824427]
    
    Signed-off-by: John Shahid <jvshahid@gmail.com>

--- tls_proxy_test.go
+++ tls_proxy_test.go
@@ -1,6 +1,7 @@
 package vizzini_test
 
 import (
+	"crypto/tls"
 	"net/http"
 
 	"code.cloudfoundry.org/bbs/models"
@@ -24,12 +25,28 @@ var _ = Describe("TLS Proxy", func() {
 	})
 
 	It("proxies traffic to the application process inside the container", func() {
-		directURL := "http://" + TLSDirectAddressFor(guid, 0, 8080)
+		directURL := "https://" + TLSDirectAddressFor(guid, 0, 8080)
 
 		resp, err := http.Get(directURL)
 		Expect(err).NotTo(HaveOccurred())
 		defer resp.Body.Close()
 
 		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+	})
+
+	It("verifies the connection cert", func() {
+		conn, err := tls.Dial("tcp", TLSDirectAddressFor(guid, 0, 8080), nil)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = conn.Handshake()
+		Expect(err).NotTo(HaveOccurred())
+
+		connState := conn.ConnectionState()
+		Expect(connState.HandshakeComplete).To(BeTrue())
+		certs := connState.PeerCertificates
+		Expect(certs).To(HaveLen(1))
+		lrp, err := ActualGetter(logger, guid, 0)()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(certs[0].Subject.CommonName).To(Equal(lrp.InstanceGuid))
 	})
 })
