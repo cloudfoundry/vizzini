3c5e3d189a0fc17c87bc8fe900fff2fe0fe6fdb7
John Shahid <jvshahid@gmail.com>
2017-10-02 13:17:23 -0400

    modify vizzini to not verify the instance identity certs when connecting
    since vizzini has to use the host ip address to connect to the container and
    the instance credential cert uses the internal ip, this has caused the http
    request and tls.Dial to fail. this change sets `InsecureSkipVerify` to `true`
    and rely on manual testing to make sure the presented certificate has valid
    fields (currently we only check for the common name but could it extended in
    the future)
    
    [#149824427]

--- tls_proxy_test.go
+++ tls_proxy_test.go
@@ -2,6 +2,7 @@ package vizzini_test
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"net/http"
 
 	"code.cloudfoundry.org/bbs/models"
@@ -27,26 +28,51 @@ var _ = Describe("TLS Proxy", func() {
 	It("proxies traffic to the application process inside the container", func() {
 		directURL := "https://" + TLSDirectAddressFor(guid, 0, 8080)
 
-		resp, err := http.Get(directURL)
+		client := http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					// the instance cred cert uses the internal container ip which causes
+					// the request to fail since we can only talk to the host ip adddress
+					InsecureSkipVerify: true,
+				},
+			},
+		}
+		resp, err := client.Get(directURL)
 		Expect(err).NotTo(HaveOccurred())
 		defer resp.Body.Close()
 
 		Expect(resp.StatusCode).To(Equal(http.StatusOK))
 	})
 
-	It("verifies the connection cert", func() {
-		conn, err := tls.Dial("tcp", TLSDirectAddressFor(guid, 0, 8080), nil)
-		Expect(err).NotTo(HaveOccurred())
+	Describe("has a valid certificate", func() {
+		var (
+			certs []*x509.Certificate
+			lrp   models.ActualLRP
+		)
 
-		err = conn.Handshake()
-		Expect(err).NotTo(HaveOccurred())
+		BeforeEach(func() {
+			conn, err := tls.Dial("tcp", TLSDirectAddressFor(guid, 0, 8080), &tls.Config{
+				// the instance cred cert uses the internal container ip which causes the
+				// request to fail since we can only talk to the host ip adddress. ignore
+				// the cert verification and do some manual assertion on the cert
+				// contents
+				InsecureSkipVerify: true,
+			})
+			Expect(err).NotTo(HaveOccurred())
 
-		connState := conn.ConnectionState()
-		Expect(connState.HandshakeComplete).To(BeTrue())
-		certs := connState.PeerCertificates
-		Expect(certs).To(HaveLen(1))
-		lrp, err := ActualGetter(logger, guid, 0)()
-		Expect(err).NotTo(HaveOccurred())
-		Expect(certs[0].Subject.CommonName).To(Equal(lrp.InstanceGuid))
+			err = conn.Handshake()
+			Expect(err).NotTo(HaveOccurred())
+
+			connState := conn.ConnectionState()
+			Expect(connState.HandshakeComplete).To(BeTrue())
+			certs = connState.PeerCertificates
+			Expect(certs).To(HaveLen(2)) // the instance identity cert + CA
+			lrp, err = ActualGetter(logger, guid, 0)()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("has a common name that matches the instance guid", func() {
+			Expect(certs[0].Subject.CommonName).To(Equal(lrp.InstanceGuid))
+		})
 	})
 })
