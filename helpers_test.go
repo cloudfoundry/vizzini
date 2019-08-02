c77f4c46326206d77a685c0ff9e7403c09c69345
Amin Jamali <ajamali@pivotal.io>
2019-08-02 18:15:58 -0400

    updated grace app
    Co-authored-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- helpers_test.go
+++ helpers_test.go
@@ -22,7 +22,7 @@ const ConvergerInterval = 30 * time.Second
 const CrashRestartTimeout = 30 * time.Second
 
 const GraceTarballURL = "https://storage.googleapis.com/diego-assets-bucket/grace.tar.gz"
-const GraceTarballURLSHAChecksum = "b98d75d7c7e3afe2d24e3c97339b15957a8f146a"
+const GraceTarballURLSHAChecksum = "35b52ecc2c305fa89d6fe37e73416359c87df0a0"
 const GraceBusyboxImageURL = "docker:///cfdiegodocker/grace"
 
 //Tasks
